2fa346cf5fddb95b2c89f3705891f9c9f8d9510b
홍경식 <ksik.hong@gmail.com>
2021-07-20 01:12:51 +0900

    UserRepositoryTest 생성

--- src/test/java/com/example/demo/entity/user/UserRepositoryTest.java
+++ src/test/java/com/example/demo/entity/user/UserRepositoryTest.java
@@ -1,21 +1,47 @@
 package com.example.demo.entity.user;
 
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
-@ExtendWith(SpringExtension.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @AfterEach
+    public void cleanUp(){
+        userRepository.deleteAll();
+    }
+
     @Test
-    public void aaa(){
-        userRepository.findAll();
+    @DisplayName("사용자 저장, 불러오기")
+    public void saveAndLoadUser(){
+        //given
+        String name = "테스트 이름";
+        String email = "testemail@gmail.com";
+        String password = "testpassword";
+
+        userRepository.save(User.builder()
+            .name(name)
+            .email(email)
+            .password(password)
+            .build());
+
+        //when
+        List<User> userList = userRepository.findAll();
+
+        //then
+        User user = userList.get(0);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getEmail()).isEqualTo(email);
+        assertThat(user.getPassword()).isEqualTo(password);
     }
 }
